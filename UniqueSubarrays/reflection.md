a66c1370cfdd80a7d9d064fe1d0d45dd57b92468
Aseel Al Dallal <aseelaldallal@gmail.com>
2017-03-28 13:51:46 -0400

    Create ArrayWeightCalculator2 that uses arrays instead of arraylist. Update reflection.

--- UniqueSubarrays/reflection.md
+++ UniqueSubarrays/reflection.md
@@ -1,11 +1,18 @@
 
 
+ArrayWeightCalculator:
 
-* In the interest of moving on to other projects, those improvements haven't been implemented. This
-is just a reflection of what I could have done better. 
+I started by writing the class ArrayWeightCalculator. The code produced the expected output in all cases, but failed the time limit test. 
+Moreover, the code wasn't written as efficiently as possible.
 
-Some improvements to the code:
-1. There was no need to use ArrayList. Array would have been sufficient
-2. Passing parentArray into ArrayWeightCalculator associates each ArrayWeightObject with a parentArrayObject. It would have been
+Some improvements that could have been made:-
+
+1. Passing parentArray into ArrayWeightCalculator associates each ArrayWeightObject with a parentArrayObject. It would have been
    better to be able to have a method such as ArrayWeightCalculator.calculateWeight(someArray) instead. 
-3. I'm a fan of having a separate class to parse user input, but I can't submit two classes to hackerearth.
+2. I'm a fan of having a separate class to parse user input, but I can't submit two classes to hackerearth.
+
+
+ArrayWeightCalculator2:
+
+I wrote this class in order to pass the time limit test on HackerEarth. I replaced ArrayList with array. I also implemented
+improvement 1. 
---
+++ UniqueSubarrays/src/ArrayWeightCalculator2.java
@@ -0,0 +1,94 @@
+
+import java.util.Scanner;
+
+public class ArrayWeightCalculator2 {
+
+	
+	// Constructor
+	public ArrayWeightCalculator2() {}
+	
+	// Pre: array is not null
+	// Post: Returns the weight of array as per specification
+	public int calculateWeight(int[] array) {
+		assert( array != null );
+		int weightTracker = 0;
+		for(int i=1; i<=array.length; i++) {
+			weightTracker += calcTotWeightOfContigousSubarraysOfLength(i, array);
+		}
+		return weightTracker;
+	}
+	
+	// Pre: length is greater than 0. array is not null
+	// Post: Finds all contigous subarrays of length 'length' in array, calculates their individual weight,
+	//       and returns the sum total of individual weights
+	public int calcTotWeightOfContigousSubarraysOfLength(int length, int[] array) {
+		assert (length > 0); 
+		assert(array != null);
+		int totalWeight = 0;
+		for(int i=0; i<=array.length - length; i++) {
+			int[] singleContigousSubarray = new int[length];
+			int subarrayPos = 0; 
+			for(int j=i; j<i+length; j++) {
+				singleContigousSubarray[subarrayPos] = array[j];
+				subarrayPos++; 
+			}
+			totalWeight += this.calculateSubarrayWeight(singleContigousSubarray);
+		}
+		
+		return totalWeight;
+	}
+	
+
+	// Pre: array is not null
+	// Post: Returns 0 if array is not unique. Returns its length otherwise
+	public int calculateSubarrayWeight(int[] array) {
+		assert(array != null);
+		if(this.isUniqueArray(array)) {
+			return array.length;
+		}
+		return 0;
+	}
+
+	// Pre: array is not null
+	// Post: Returns true if each element in the array appears only once
+	public boolean isUniqueArray(int[] array) {		
+		assert(array != null);	
+		for(int i=0; i<array.length-1; i++) {
+			for(int j=i+1; j<array.length; j++) {
+				if( array[i] == array[j]) {
+					return false;
+				}
+			}
+		}		
+		return true;
+	}
+	
+	
+	/*---------------- MAIN --------------*/
+	public static void main (String[] args) throws Exception {
+	
+        //Scanner
+        Scanner textInputScanner = new Scanner(System.in);
+        int noTestCases = Integer.parseInt(textInputScanner.nextLine());
+
+        ArrayWeightCalculator2 myCalc = new ArrayWeightCalculator2();
+        
+        //Read Input
+        for (int i=0; i < noTestCases; i++) {
+            int arraySize = Integer.parseInt(textInputScanner.nextLine()); 
+            int[] numbers = new int[arraySize];
+            String arrayLine = textInputScanner.nextLine();           
+            String[] numbersStringArray = arrayLine.split(" ");       
+            for(int j=0; j<arraySize; j++) {
+            	numbers[j] = Integer.parseInt(numbersStringArray[j]); 
+            }
+            System.out.println(myCalc.calculateWeight(numbers));
+        }
+        textInputScanner.close(); 
+       
+    
+   
+
+	
+	}
+}
